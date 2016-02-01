c990b8858848be31f7ab3eff50a42576a03ea84a
Jason Noble <github+jasonn@jasonnoble.org>
2016-01-31 18:04:04 -0700

    Initial commit

---
+++ Tap Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tap Me
+//
+//  Created by Jason Noble on 1/31/16.
+//  Copyright © 2016 Jason Noble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
